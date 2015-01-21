d8b5e96fe7be13362daf9224397ca03340713e60
panxinx <panxinx@aol.com>
2015-01-21 11:47:53 -0800

    update

--- src.js
+++ src.js
@@ -19,21 +19,41 @@
 
 
 
-var arr = new Array(3);
+//var arr = new Array(3);
 //arr.push("XXX");
 //arr.push("YYY");
 //arr.push("ZZZ");
-arr.push("WWWW");
-arr.push("TTTT");
-arr[0] = "AAAA";
-arr[1] = "BBBB";
-arr[2] = "CCCC";
-arr[3] = "ZZZZ";
+//arr.push("WWWW");
+//arr.push("TTTT");
+//arr[0] = "AAAA";
+//arr[1] = "BBBB";
+//arr[2] = "CCCC";
+//arr[3] = "ZZZZ";
 
-var str = arr.length;
+//var str = arr.length;
+//
+//for(var index = 0; index < arr.length; index++) {
+//    str = str + "/" + arr[index];
+//}
 
-for(var index = 0; index < arr.length; index++) {
-    str = str + "/" + arr[index];
+//alert(str);
+
+
+var golf = {
+    make: "Volkswagen",
+    model: "GTI",
+    year: "2012",
+    color: "White",
+    VIN: "123456789ABC"
 }
 
-alert(str);
+//alert(golf.color);
+
+golf.doorNum = 2;
+delete golf.year;
+alert(golf.year);
+
+
+
+
+
