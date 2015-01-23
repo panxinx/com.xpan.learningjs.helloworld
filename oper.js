9f69dafdef112e3d6047bee54e443d422a07b98a
panxinx <panxinx@aol.com>
2015-01-22 18:15:01 -0800

    update

--- oper.js
+++ oper.js
@@ -12,3 +12,61 @@ function changeValue() {
     var value = document.getElementById("code_1");
     value.innerHTML = "Hello JavaScript.";
 }
+
+//alert(isNaN(10));
+
+
+//var a = Math.sqrt(-1);
+
+//alert(3 + Number("abc"));
+
+//var str = String("xyz");
+
+
+//var str = "abcdefgh";
+//alert(str.charAt(0));
+//alert(str.charAt(100));
+//alert(str[1]);
+//alert(str[100]);
+
+
+function validatePhoneNumber(phoneNum) {
+    if (phoneNum == null || phoneNum == "") {
+        return false;
+    }
+
+    if(phoneNum.length != 8){
+        return false;
+    }
+
+    var parts = phoneNum.split("-");
+    if (parts.length != 2) {
+        return false;
+    }
+
+    if(parts[0].length != 3 && parts[1].length != 4) {
+        return false;
+    }
+
+    if (!isNaN(Number(parts[0])) && !isNaN(Number(parts[1]))) {
+        return true;
+    }
+
+    return false;
+}
+
+function showValidationResult() {
+    var access = document.getElementById("telNum");
+    var phoneNum = access.getAttribute("value");
+    if (validatePhoneNumber(phoneNum)) {
+        alert("It's a valid number.");
+    } else {
+        alert("It's an invalid number.");
+    }
+}
+
+
+var a = 111;
+var b = typeof true;
+alert(b);
+
